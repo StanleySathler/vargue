20450915ecc37d9b3dcdeb5c2ab25bc7908a085c
StanleySathler <stanleysathlerpinto@gmail.com>
2020-05-21 22:30:29 -0300

    fix: make english the date filter

--- src/filters.js
+++ src/filters.js
@@ -1,7 +1,6 @@
 const format = require('date-fns/format');
-const br = require('date-fns/locale/pt-BR');
 const Sqrl = require('squirrelly-fork');
 
 Sqrl.defineFilter('date', date => {
-  return format(date, 'dd MMM yyyy', { locale: br });
+  return format(date, 'dd MMM yyyy');
 });
